a3108edad6c28eaafff8ebd4e1b649a405df6cfc
qikai-jin_msbugh <qikai_jin@nec.com.sg>
2026-05-29 18:57:28 +0800

    Revert "Temporary destory the env"
    This reverts commit f1d7539de278988728709aadd89fe6d70d693ca0.

--- terraform/environments/dev/backend.tf
+++ terraform/environments/dev/backend.tf
@@ -1,18 +1,18 @@
 # Remote state on S3 with DynamoDB locking
 # Requires pre-existing S3 bucket + DynamoDB table + IAM permissions.
-terraform {
-  backend "s3" {
-    bucket         = "notes-api-tf-state-ap-southeast-1"
-    key            = "dev/terraform.tfstate"
-    region         = "ap-southeast-1"
-    dynamodb_table = "terraform-state-lock"
-    encrypt        = true
-  }
-}
-
-# Local state for testing and development
 # terraform {
-#   backend "local" {
-#     path = "terraform.tfstate"
+#   backend "s3" {
+#     bucket         = "notes-api-tf-state-ap-southeast-1"
+#     key            = "dev/terraform.tfstate"
+#     region         = "ap-southeast-1"
+#     dynamodb_table = "terraform-state-lock"
+#     encrypt        = true
 #   }
 # }
+
+# Local state for testing and development
+terraform {
+  backend "local" {
+    path = "terraform.tfstate"
+  }
+}
